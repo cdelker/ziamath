cece1476154e6f0f5400aac6e91ae7b4781125c8
Collin Delker <code@collindelker.com>
2023-05-28 09:34:46 -0600

    Version 0.8

--- ziamath/__init__.py
+++ ziamath/__init__.py
@@ -3,7 +3,7 @@ from .styles import styledchr, styledstr
 from .zmath import Math, Latex, Text, declareoperator
 from .config import config
 
-__version__ = '0.8a0'
+__version__ = '0.8'
 
 
 declareoperator(r'\tg')

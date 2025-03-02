794ad2acdd56741fcce348a9982b62dfe9025e44
Collin Delker <code@collindelker.com>
2025-03-02 08:49:10 -0700

    Version 0.12

--- ziamath/__init__.py
+++ ziamath/__init__.py
@@ -4,7 +4,7 @@ from .zmath import Math, Latex, Text, reset_numbering
 from .tex import declareoperator
 from .config import config
 
-__version__ = '0.12a0'
+__version__ = '0.12'
 
 
 declareoperator(r'\tg')
